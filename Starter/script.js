82b3fe85ed1832e2cd95dcd530441a8a9cda4b69
Tamsin Lloyd <tamsin_lloyd@hotmail.com>
2024-01-25 21:37:15 +0000

    left space in API for own key

--- Starter/script.js
+++ Starter/script.js
@@ -1,4 +1,4 @@
 $("button").on("click", function () {
     
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=58be503d-e5d2-43a5-8bef-909ba7e71702";
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=[your API key]";
 });
