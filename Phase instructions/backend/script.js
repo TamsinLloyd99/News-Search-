8645fb1676750dfc7c698c39f677632220af6f56
Victor <victormarquesrina@hotmail.com>
2024-01-26 01:00:24 +0000

    Function to fetch articles

---
+++ Phase instructions/backend/script.js
@@ -0,0 +1,10 @@
+// Function to fetch articles
+function fetchArticles(event) {
+    event.preventDefault();
+
+    const apiKey = 'tEvWUBAbAo148kYEMhwAf9L4GtbNsYG9'; 
+    const query = document.getElementById('search-term').value;
+    const numRecords = document.getElementById('num-records-select').value;
+    const startDate = document.getElementById('start-date').value;
+    const endDate = document.getElementById('end-date').value;
+}
